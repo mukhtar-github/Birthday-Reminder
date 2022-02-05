f0d65c51f63acd89b53c1c1914debb702ce60967
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-05 15:15:11 +0100

    Update file

--- birthday-app/src/App.js
+++ birthday-app/src/App.js
@@ -1,8 +1,19 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [poeple, setPeople] = useState(data);
+
+  return (
+    <main>
+      <section className='container'>
+        <h3>{ poeple.length } birthdays today</h3>
+        <List poeple={poeple} />
+        <button onClick={ () => setPeople([]) }>clear all</button>
+      </section>
+    </main>
+  );
 }
 
 export default App;
