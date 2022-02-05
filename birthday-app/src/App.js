792c84e72807b0a1554385d2440239f467896a9e
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-05 15:23:00 +0100

    Update files

--- birthday-app/src/App.js
+++ birthday-app/src/App.js
@@ -3,13 +3,13 @@ import data from './data';
 import List from './List';
 
 function App() {
-  const [poeple, setPeople] = useState(data);
+  const [people, setPeople] = useState(data);
 
   return (
     <main>
       <section className='container'>
-        <h3>{ poeple.length } birthdays today</h3>
-        <List poeple={poeple} />
+        <h3>{ people.length } birthdays today</h3>
+        <List people={people} />
         <button onClick={ () => setPeople([]) }>clear all</button>
       </section>
     </main>
--- birthday-app/src/List.js
+++ birthday-app/src/List.js
@@ -3,7 +3,7 @@ import React from 'react';
 const List = () => {
   return (
     <>
-      <h2>list component</h2>
+      <h2>list Asmau</h2>
     </>
   );
 };
